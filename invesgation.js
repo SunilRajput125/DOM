526cd42f58de914a5f2019bdb1f5b224416fdaed
Sunil Rajput <sr3222390@gmail.com>
2024-08-27 06:48:42 -0700

    coversion of datatypes

---
+++ invesgation.js
@@ -0,0 +1,28 @@
+/*let score = "sunil"
+
+
+console.log(typeof score);
+
+let ValueInNumber = Number(score);
+
+
+
+console.log(typeof ValueInNumber);
+
+console.log(ValueInNumber);*/
+
+
+// "33" => 33 number
+// "33abc" => NaN but this type of number lit confused
+// true => 1 false => 0
+
+// Boolean
+
+let IsLoggedIn = "222"
+
+let BooleanIsLoggedIn = Boolean(IsLoggedIn)
+
+console.log(BooleanIsLoggedIn);
+
+// 1 => true 0 => false
+// "" => fasle => true
