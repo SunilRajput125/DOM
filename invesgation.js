47f02508ba6d75aac6e6b97bf462624e371b628f
Sunil Rajput <sr3222390@gmail.com>
2024-08-30 05:55:09 -0700

    operation.js

--- invesgation.js
+++ invesgation.js
@@ -22,7 +22,49 @@ let IsLoggedIn = "222"
 
 let BooleanIsLoggedIn = Boolean(IsLoggedIn)
 
-console.log(BooleanIsLoggedIn);
+// console.log(BooleanIsLoggedIn);
 
 // 1 => true 0 => false
-// "" => fasle => true
+// "" => fasle => 
+
+let Name = "Sunil";
+
+let Changedstr = (Name);
+
+// console.log(Changedstr);
+
+
+// ************************** Operation *******************************
+
+
+let speed = 120;
+
+for (let Hi_speed = 120; Hi_speed < 200; Hi_speed++) {
+
+    // console.log(speed + Hi_speed);
+
+};
+
+// **Addition**//
+
+let Str1 = 2;
+
+let Str2 = 2;
+
+let Add = (Str1 + Str2);
+
+// console.log(Add);
+
+
+// *********** %% *******************
+
+let Num1 = 1342;
+
+let Num2 = 20;
+
+let Percent = (Num1 % Num2);
+
+console.log(Percent);
+
+
+
