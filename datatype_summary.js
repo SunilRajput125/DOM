109735d5397d6114752968df0f0959adefb09d85
Sunil Rajput <sr3222390@gmail.com>
2024-08-31 01:15:12 -0700

    Datatype of js

---
+++ datatype_summary.js
@@ -0,0 +1,49 @@
+//Primitive datatype
+
+// 7 Type of ....
+// 1 String
+// 2 Number
+// 3 Boolearn
+// 4 Null
+// 5 Undefined
+// 6 Symbol
+// 7 Bigint
+
+const score = 100;
+const scoreValue = 100.4;
+
+const isLogged = false;
+const outsidetemp = null;
+
+let userEmail; // undefined
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+// console.log(id === anotherId);
+
+
+// const bigNumber = 2453435534322445n;
+
+const heros = ["supermen", "spridermen", "ironmen"];
+
+let myyobj =
+{
+    Name: "sunil",
+    age: 22.
+}
+const myfunc = function(){
+    // console.log("Hello world");
+    
+}
+
+console.log(typeof undefined)
+
+
+
+
+
+// Reference (Non Primitive)
+
+
+// Array, Objects, Functions
