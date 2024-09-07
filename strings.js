c552ad0a0ce2bc774052c1ea4b8f97c2f176a8eb
Sunil Rajput <sr3222390@gmail.com>
2024-09-07 07:02:43 -0700

    summary of strings

---
+++ strings.js
@@ -0,0 +1,23 @@
+
+// string concat
+"Sunil" + "Rajput"
+
+"Hello world"
+
+
+const name = "Sumil"
+const repoCount = 56
+
+console.log( name + repoCount + "Value");
+
+let newOne = "Noman"
+
+let oldOne = "Samason"
+
+let thirdOne = (`Sunil and ${newOne} and  ${oldOne} are bestfrnd`);
+
+console.log(newOne,oldOne,thirdOne.slice(8));
+
+
+
+
